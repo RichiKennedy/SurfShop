3ba63a758d373d0a6cc1af3bbc88c1fd124096ea
richard <richardgrahamkennedy@gmail.com>
2023-04-28 09:46:14 +0200

    create layout for nav and footer using outlet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom"
 import SingleProduct from "./pages/Product/SingleProduct"
 import Products from "./pages/Products/Products";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Home />
-  },
-  {
-    path:"/products/:id",
-    element:<Products />
-  },
-  {
-    path:"/product/:id",
-    element:<SingleProduct />
-  },
+    element:<Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <SingleProduct />
+      },
+    ]
+  }
 ])
 
 function App() {
