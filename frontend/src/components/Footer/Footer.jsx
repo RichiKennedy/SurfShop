6c70d8a75eb35651cb7f450310eed51ad7f05a15
richard <richardgrahamkennedy@gmail.com>
2023-05-02 13:50:08 +0200

    Create footer with wave animation

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Footer.scss'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='waves'>
+        <div className='wave' id='wave1'></div>
+        <div className='wave' id='wave2'></div>
+        <div className='wave' id='wave3'></div>
+        <div className='wave' id='wave4'></div>
+      </div>
+      <ul className='social-icons'>
+        <li><a className='link' href='google.com' target='_blank'> <FacebookOutlinedIcon /> </a></li>
+        <li><a className='link' href='google.com' target='_blank'> <FacebookOutlinedIcon /> </a></li>
+        <li><a className='link' href='google.com' target='_blank'> <FacebookOutlinedIcon /> </a></li>
+        <li><a className='link' href='google.com' target='_blank'> <FacebookOutlinedIcon /> </a></li>
+      </ul>
+      <ul className='menu'>
+        <li><Link className='link' to="/"> Home </Link></li>
+        <li><Link className='link' to="/"> About </Link></li>
+        <li><Link className='link' to="/"> Contact </Link></li>
+        <li><Link className='link' to="/"> Stores </Link></li>
+      </ul>
+    </footer>
   )
 }
 
