0bdde1d9a1aca543201429851599e46c3157513a
richard <richardgrahamkennedy@gmail.com>
2023-06-05 15:07:36 +0200

    feature/ create header with category filter option

---
+++ frontend/src/components/FilterDropDown/FilterDropDown.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FilterDropDown.scss'
+
+const FilterDropDown = () => {
+  return (
+    <div>FilterDropDown</div>
+  )
+}
+
+export default FilterDropDown
--- frontend/src/pages/Products/Products.jsx
+++ frontend/src/pages/Products/Products.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Products.scss'
+import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
+import { Link } from 'react-router-dom';
+import FilterDropDown from '../../components/FilterDropDown/FilterDropDown';
 
 const Products = () => {
+  const [open, setOpen] = useState(false)
+  const [cssStyles, setCSSStyles] = useState({
+    position: 'relative',
+  })
+console.log(window.scrollY)
+useEffect(() => {
+  const changePosition = () => {
+    if (window.scrollY >= 100) {
+      setCSSStyles({
+        position: 'fixed'
+      })
+    } else {
+      setCSSStyles({
+        position: 'relative',
+      })
+    }
+  }
+  window.addEventListener('scroll', changePosition)
+})
   return (
-    <div className='products'>Products</div>
+    <div className='products'>
+      <div className="header">
+        <h3> New Arrivals</h3>
+      </div>
+      <div 
+      className="filter-wrapper"
+      style={{position: cssStyles.position}}
+      >
+        <div className="product-count"> <span>116</span> <span>items</span> </div>
+        <Link onClick={ () => setOpen(!open)}> 
+         <div className="filter">
+          <h6> <FilterListOutlinedIcon className='filter-icon' /> filter </h6>
+          </div>
+          </Link> 
+          {open && <FilterDropDown />}
+      </div>
+    </div>
   )
 }
 
