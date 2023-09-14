53afe6335fe03f4a31492a17ee7630932f133a1d
richard <richardgrahamkennedy@gmail.com>
2023-09-14 19:08:14 +0200

    feature: add useContext to manage state

---
+++ frontend/src/Context/cartContext.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+    const [openCart, setOpenCart] = useState(false);
+    const [isNewProductAdded, setIsNewProductAdded] = useState(false);
+    const products = useSelector(state => state.cart.products)
+
+    useEffect(() => {
+        if (products.length > 0 && isNewProductAdded) {
+          setOpenCart(true);
+          
+          const timer = setTimeout(() => {
+            setIsNewProductAdded(false);
+          }, 1000);
+          
+          return () => clearTimeout(timer);
+        }
+      }, [products]);
+
+  return (
+    <AppContext.Provider value={{ 
+        openCart, 
+        setOpenCart,
+        isNewProductAdded,
+        setIsNewProductAdded,
+        }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
