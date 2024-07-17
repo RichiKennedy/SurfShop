95fc41252efc1cba4c7eb14c8b1393c3de7ce403
richard <richardgrahamkennedy@gmail.com>
2024-07-17 10:39:54 +0200

    basic design of accounts page

--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
 import './Account.scss';
 import { useNavigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAuthContext } from '../../Context/authContext';
 import useFetch from '../../Hooks/useFetch';
+import moment from 'moment';
+
+const calculateTotalPrice = (products) => {
+  return products.reduce((sum, product) => sum + product.price, 0);
+};
 
 const Account = () => {
   const navigate = useNavigate();
   const { user } = useAuthContext();
 
   const filterQuery = user ? `/orders?filters[user][$eq]=${user.id}` : null;
-
-  console.log('Fetching with query:', filterQuery); 
 
   const { data: orders, loading, error } = useFetch(filterQuery, true);
 
@@ -22,11 +26,11 @@ const Account = () => {
   }, [user, navigate]);
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   if (!user) {
-    return null; 
+    return null;
   }
 
   const navigateToLogout = () => {
@@ -34,55 +38,66 @@ const Account = () => {
   };
 
   return (
-    <div className='account-wrapper'>
-      <div className="account-info-wrapper">
-        <h2>Account Information</h2>
-        <ul>
-          <li>
-            <span>Username:</span>
-            <h6>{user.username}</h6>
-          </li>
-          <li>
-            <span>Email:</span>
-            <h6>{user.email}</h6>
-          </li>
-        </ul>
+    <motion.div
+     className="accounts-page-wrapper"
+     initial={{ opacity: 0 }}
+     animate={{ opacity: 1 }}
+     exit={{ opacity: 0 }}
+     transition={{ duration: 0.3 }}>
+      <div className="account-wrapper">
+        <div className="account-info-wrapper">
+          <h2>Account Information</h2>
+          <ul>
+            <li>
+              <span>Username:</span>
+              <h6>{user.username}</h6>
+            </li>
+            <li>
+              <span>Email:</span>
+              <h6>{user.email}</h6>
+            </li>
+          </ul>
+          <button onClick={navigateToLogout}>Logout</button>
+        </div>
+      </div>
+      <div className="orders-wrapper">
         <h2>Your Orders</h2>
-      </div>
-        <>
-      <div>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error.message}</p>}
-            <ul>
-            {orders && orders.map((order) => (
-                    <div className="order-container" key={order.id}>
-                      <div className="order-details">
-                        <h3>Order ID: {order.id}</h3>
-                        <h4>Order Date: {order.createdAt}</h4>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error.message}</p>}
+        <ul>
+          {orders && orders.map((order) => {
+            const total = calculateTotalPrice(order.attributes.products);
+            return (
+              <div className="order-container" key={order.id}>
+                <div className="order-details">
+                  <h3>Order reference: {order.id}</h3>
+                  <h4>Order Date: {moment(order.attributes.createdAt).format('MMMM Do, YYYY')}</h4>
+                </div>
+                <div className="products-container">
+                  {order.attributes.products.map((product) => (
+                    <div className="product-container" key={product.id}>
+                      <div className="product-img-wrapper">
+                        {product.img && (
+                          <img src={process.env.REACT_APP_UPLOAD_URL + product.img} alt={product.title} />
+                        )}
                       </div>
-                      <div className="products-container">
-                        {order.attributes.products && order.attributes.products.map((product) => (
-                          <div className="product-container" key={product.id}>
-                            <div className="product-img-wrapper">
-                              {product.img && (
-                                <img src={process.env.REACT_APP_UPLOAD_URL + product.img} alt={product.title} />
-                              )}
-                            </div>
-                            <div className="product-details">
-                              <h4>{product.title}</h4>
-                              <p>Price: ${product.price}</p>
-                              {/* Add more details as needed */}
-                            </div>
-                          </div>
-                        ))}
+                      <div className="product-details">
+                        <h4>{product.title}</h4>
+                        <p className='product-desc'>{product.desc}</p>
+                        <p>Price: ${product.price}</p>
                       </div>
-                    </div> 
-                ))}
-            </ul>
+                    </div>
+                  ))}
+                </div>
+                <div>
+                  <h4>Total: ${total.toFixed(2)}</h4> 
+                </div>
+              </div>
+            );
+          })}
+        </ul>
       </div>
-        </>
-      <button onClick={navigateToLogout}>Logout</button>
-    </div>
+    </motion.div>
   );
 };
 
